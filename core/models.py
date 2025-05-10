43232988bf2beec662dd24ad7ea4a3379a0a99c1
luscapelli <alcantaraminacapellilucas@gmail.com>
2025-05-10 15:09:12 -0300

    Inicio do Backend do Projeto

---
+++ core/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    imagem = models.ImageField(upload_to='produtos/')  # Para salvar imagens dos produtos
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
+
+class Servico(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    imagem = models.ImageField(upload_to='servicos/')  # Para salvar imagens dos serviços
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
